0c05afcfd06c19efd89ed6c0acf016b6313e828b
@equinox79 <junichiro79@gmail.com>
2013-06-15 11:02:39 +0900

    import files from minilla

---
+++ lib/Pororoca.pm
@@ -0,0 +1,39 @@
+package Pororoca;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Pororoca - It's new $module
+
+=head1 SYNOPSIS
+
+    use Pororoca;
+
+=head1 DESCRIPTION
+
+Pororoca is ...
+
+=head1 LICENSE
+
+Copyright (C) @equinox79.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+@equinox79 E<lt>junichiro79@gmail.comE<gt>
+
+=cut
+
